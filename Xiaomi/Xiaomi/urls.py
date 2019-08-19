5c690b8000fe6b6c2969e1fc3da4255301f4fdc0
feifei <1322877537@qq.com>
2019-08-19 20:36:14 +0800

    XIAOMI

--- Xiaomi/Xiaomi/urls.py
+++ Xiaomi/Xiaomi/urls.py
@@ -1,4 +1,4 @@
-"""Xiaomi URL Configuration
+"""xiaomi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^app/', include('login.urls', namespace='app'))
+    url(r'^app/', include('App.urls', namespace='app'))
 ]
--- Xiaomi/Xiaomi/wsgi.py
+++ Xiaomi/Xiaomi/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Xiaomi project.
+WSGI config for xiaomi project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Xiaomi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xiaomi.settings")
 
 application = get_wsgi_application()
